69e18e734685820f665242e66e8fd4cc38a2b87b
Kalle Kromann <kallekro@gmail.com>
2018-09-24 22:46:34 +0200

    listener docu

--- src/listener.py
+++ src/listener.py
@@ -32,47 +32,80 @@ class Listener(object):
             worker.start()
 
     def kill(self):
+        """
+        Kill the server.
+        """
         self.stop_listen()
-        self.kill_pool = True
-        self.job_queue.join()
-
-    def _receiveMsg(self):
-        time.sleep(0.0001)
-        while True:
-            if self.kill_pool:
-                return
-            self.pool_cond.acquire()
-            while self.job_queue.empty():
-                self.pool_cond.wait()
-            data, addr = self.job_queue.get(block=False)
-            self.receiveMsg(data, addr)
-            self.job_queue.task_done()
-            self.pool_cond.release()
-
-    def receiveMsg(self, data, addr):
-        print("received: %s from %s" % (data, addr))
+        self._kill_pool()
 
     def listen(self):
+        """
+        Start listen thread.
+        """
         self.listen_thread = Thread(target=self._listen_thread)
         self.listen_thread.start()
 
     def stop_listen(self):
+        """
+        Stop listen thread.
+        """
         self.listener_lock.acquire()
-        self.kill_listen = True
+        self.kill_listen = True # This will cause the listen thread to exit
         self.listener_lock.release()
         self.listen_thread.join()
 
+    def receiveMsg(self, data, addr):
+        """
+        Public method - should be overwritten.
+        """
+        print("received: %s from %s" % (data, addr))
+
+    def _receiveMsg(self):
+        """
+        Private method - should not be overwritten or called from outside the class.
+        This method is run by the worker threads.
+        """
+        time.sleep(0.0001)
+        while True:
+            self.pool_cond.acquire() # lock mutex
+            while self.job_queue.empty():
+                if self.kill_pool:
+                    return # return if pool is being killed
+                self.pool_cond.wait() # unlock mutex and wait for cond broadcast
+            job = self.job_queue.get(block=False) # get the first job in the queue (don't block)
+            if not job: continue # if you didn't get the job anyways
+            data, addr = job # unpack job
+            self.receiveMsg(data, addr) # call receive with data
+            self.job_queue.task_done() # notify the queue that the task is done
+            self.pool_cond.release() # unlock mutex
+
     def _listen_thread(self):
+        """
+        Private method - should not be called from outside the class.
+        The listen thread calls this function.
+        Listens for new packets and adds them to job queue.
+        """
         while 1:
-            time.sleep(0.0001) # without this the lock will be reacquired almost every time
+            time.sleep(0.0001) # without this the lock will be reacquired almost every time (leaving no time for killing the thread)
             self.listener_lock.acquire()
             if self.kill_listen:
-                return
-            ready = select.select([self.sock], [], [], TIMEOUT)
-            if ready[0]:
+                return # return if listener is being killed
+            ready = select.select([self.sock], [], [], TIMEOUT) # poll the socket
+            if ready[0]: # if packets
                 data, addr = self.sock.recvfrom(4096)
                 self.pool_cond.acquire()
-                self.job_queue.put((data.decode(), addr))
-                self.pool_cond.notify_all()
+                self.job_queue.put((data.decode(), addr)) # add packet to job queue
+                self.pool_cond.notify_all() # notify workers
                 self.pool_cond.release()
             self.listener_lock.release()
+
+    def _kill_pool(self):
+        """
+        Private method - should not be called from outside the class.
+        Kills the job queue
+        """
+        self.pool_cond.acquire()
+        self.kill_pool = True
+        self.job_queue.join()
+        self.pool_cond.notify_all()
+        self.pool_cond.release()
