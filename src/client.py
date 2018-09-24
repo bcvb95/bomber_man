4bf8babc89aa2cf92d80a75030f768ff8563de88
Kalle Kromann <kallekro@gmail.com>
2018-09-24 18:37:58 +0200

    yaws

--- src/client.py
+++ src/client.py
@@ -12,35 +12,35 @@ class TestServer(Listener):
     def __init__(self, ip, port):
         Listener.__init__(self, ip, port)
 
-    def sendMsg(self, msg):
-        self.sock.sendto(msg.encode(), (self.ip, self.port))
+    def sendMsg(self, msg, to_ip, to_port):
+        self.sock.sendto(msg.encode(), (to_ip, to_port))
 
     def receiveMsg(self, data):
         print("SERVER received: %s" % data)
 
 
 class Client(Listener):
-    def __init__(self, ip, port):
+    def __init__(self, ip, port, serverIP, serverPort):
         Listener.__init__(self, ip, port)
+        self.serverIP = serverIP
+        self.serverPort = serverPort
 
     def sendMsg(self, msg):
-        self.sock.sendto(msg.encode(), (self.ip, self.port))
+        self.sock.sendto(msg.encode(), (self.serverIP, self.serverPort))
 
     def receiveMsg(self, data):
         print("CLIENT received: %s" % data)
 
-
-
 def test_client():
     server = TestServer("127.0.0.1", SERVER_PORT)
     server.listen()
-    client = Client("127.0.0.1", CLIENT_PORT)
+    client = Client("127.0.0.1", CLIENT_PORT, "127.0.0.1", SERVER_PORT)
     client.listen()
     client.sendMsg("lol")
     time.sleep(1)
-    client.sendMsg("hej")
+    server.sendMsg("hej", "127.0.0.1", SERVER_PORT)
     time.sleep(1)
-    server.sendMsg("YUP")
+    server.sendMsg("YUP", "127.0.0.1", CLIENT_PORT)
     time.sleep(1)
     server.stop_listen()
     client.stop_listen()
