e782065ea495f84e8f934dcb25f228969136aac2
Bjørn <bjornvedel95@gmail.com>
2018-09-25 13:05:46 +0200

    smallo

--- src/misc.py
+++ src/misc.py
@@ -1,6 +1,14 @@
 
 def listToStringParser(lst, seperator=','):
+    if len(lst) == 0:
+        return ""
     return ("%s" % seperator).join(lst) 
 
 def stringToListParser(string, seperator=','):
+    if len(string) == 0:
+        return []
     return string.split(seperator)
+
+def recentMovesToStringParser(recent_moves, seperator=','):
+    moves = [m[0] for m in recent_moves]
+    return ("%s" % seperator).join(moves)
