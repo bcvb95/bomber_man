148d9b15cd8c6e18cd44aec5ccb2c936825d7544
Kalle Kromann <kallekro@gmail.com>
2018-10-01 13:51:02 +0200

    gamemanager class

--- bomberman_src/bomberman_main.py
+++ bomberman_src/bomberman_main.py
@@ -7,128 +7,138 @@ from bomberman_consts import *
 from bomberman_player import BMPlayer
 from movable_gameobject import MoveableGameObject
 
-def start_game(username, client_port, server_ip, server_port, is_server):
-    # init pygame and screen
-    pygame.init()
-    pygame.display.set_caption("Bomberman Pygame-edition")
-    screen = pygame.display.set_mode(SCREEN_SIZE)
+class GameManager(object):
+    def __init__(self, username, client_port, server_ip, server_port, is_server):
+        self.username = username
+        self.client_port = client_port
+        self.server_ip = server_ip
+        self.server_port = server_port
+        self.is_server = is_server
 
-    # load images
-    player_char_img_dict = res_loader.load_player_images()
+        # input
+        self.queued_dir_input = (0,0)
+        self.dir_input = (0,0)
+        self.move_key_held = None
+        self.do_move = False
 
-    client_ip = misc.getMyIP()
-    player = None
+        # init pygame and screen
+        pygame.init()
+        pygame.display.set_caption("Bomberman Pygame-edition")
+        self.screen = pygame.display.set_mode(SCREEN_SIZE)
 
-    # instantiate player
-    if is_server:
-        player = BMPlayer(username,client_ip, client_port, server_ip, server_port, is_server)
-        player.server.startBroadcasting()
-    else:
-        player = BMPlayer(username,client_ip, client_port, server_ip, server_port)
-        player.client.logIn()
+        # load images
+        player_char_img_dict = res_loader.load_player_images()
 
-    # wait for the client to log into the server
-    while not player.client.logged_in:
-        time.sleep(0.001)
+        client_ip = misc.getMyIP()
+        self.player = None
 
-    #setup gameboard
-    gameboard = GameBoard(GRID_SIZE)
+        # instantiate player
+        if is_server:
+            self.player = BMPlayer(username,client_ip, client_port, server_ip, server_port, is_server)
+            self.player.server.startBroadcasting()
+        else:
+            self.player = BMPlayer(username,client_ip, client_port, server_ip, server_port)
+            self.player.client.logIn()
 
-    #TODO Instatiate clients player_model
+        # wait for the client to log into the server
+        while not self.player.client.logged_in:
+            time.sleep(0.001)
 
-    # instantiate THIS players character, which is object that is being drawn
-    player_char = MoveableGameObject(STEPSIZE, player_char_img_dict[player.client.player_number])
+        #setup gameboard
+        self.gameboard = GameBoard(GRID_SIZE)
 
-    player_i = player.client.player_number-1
-    start_i, start_j =PLAYER_START_IDX_POSITIONS[player_i]
-    start_x, start_y = (TILE_SIZE+ start_i*TILE_SIZE),  (TILE_SIZE + start_j*TILE_SIZE)
-    player_char.grid_pos = (start_i, start_j)
-    player_char.scr_pos = (start_x, start_y)
-    player_char.set_pos(start_x, start_y)
+        #TODO Instatiate clients player_model
 
-    start_game = False
-    if player.is_server:
-        # wait for host to start the game, when it chooses to.
-        print("\n\nWaiting for players to join.\n Press \"S\" to start game.\n\n")
+        # instantiate THIS players character, which is object that is being drawn
+        self.player_char = MoveableGameObject(STEPSIZE, player_char_img_dict[self.player.client.player_number])
 
-        num_connected = len(player.server.connected_clients)
-        print("> %d/4 number of players connected." % num_connected)
-        while not start_game:
-            if len(player.server.connected_clients) != num_connected:
-                num_connected = len(player.server.connected_clients)
-                print("> %d/4 number of players connected" % num_connected)
+        player_i = self.player.client.player_number-1
+        start_i, start_j =PLAYER_START_IDX_POSITIONS[player_i]
+        start_x, start_y = (TILE_SIZE+ start_i*TILE_SIZE),  (TILE_SIZE + start_j*TILE_SIZE)
+        self.player_char.grid_pos = (start_i, start_j)
+        self.player_char.scr_pos = (start_x, start_y)
+        self.player_char.set_pos(start_x, start_y)
 
-            for event in pygame.event.get():
-                if (event.type == KEYDOWN and event.key == K_s) or num_connected == 4:
-                    start_game = True
-                    print("> Starting game!")
-    else:
-        print("\n\nWaiting for the host to start the game. DEBUG: press s to start anyway\n")
-        while not start_game:
-            for event in pygame.event.get():
-                if (event.type == KEYDOWN and event.key == K_s):
-                    start_game = True
+    def start_game(self):
+        start_game_flag = False
+        if self.player.is_server:
+            # wait for host to start the game, when it chooses to.
+            print("\n\nWaiting for players to join.\n Press \"S\" to start game.\n\n")
 
+            num_connected = len(self.player.server.connected_clients)
+            print("> %d/4 number of players connected." % num_connected)
+            while not start_game_flag:
+                if len(self.player.server.connected_clients) != num_connected:
+                    num_connected = len(self.player.server.connected_clients)
+                    print("> %d/4 number of players connected" % num_connected)
 
-    queued_dir_input = (0,0)
-    dir_input = (0,0)
+                for event in pygame.event.get():
+                    if (event.type == KEYDOWN and event.key == K_s) or num_connected == 4:
+                        start_game_flag = True
+                        print("> Starting game!")
+        else:
+            print("\n\nWaiting for the host to start the game. DEBUG: press s to start anyway\n")
+            while not start_game_flag:
+                for event in pygame.event.get():
+                    if (event.type == KEYDOWN and event.key == K_s):
+                        start_game_flag = True
+        self.game_loop()
 
-    game_running = True
-    while game_running:
-        do_move = (time.time() - player_char.last_move > MINMOVEFREQ)
+    def game_loop(self):
+        game_running = True
+        while game_running:
+            self.handle_input()
+            self.update()
+            self.draw()
+
+    def handle_input(self):
+        self.do_move = (time.time() - self.player_char.last_move > MINMOVEFREQ)
         for event in pygame.event.get():
             exit_cond = (event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE))
             if exit_cond: # If closing game
-                if player.is_server:
-                    player.server.stopBroadcasting()
-                player.kill()
+                if self.player.is_server:
+                    self.player.server.stopBroadcasting()
+                self.player.kill()
                 sys.exit()
 
             #---- move specific input ----#
             move = None
             if event.type == pygame.KEYDOWN:
-                if event.key in DIRECTION_INPUT_DICT:
-                    dir_input = DIRECTION_INPUT_DICT[event.key]
-                    move = dir_input
+                if event.key in KEY_TO_DIR_DICT and not self.move_key_held:
+                    self.dir_input = KEY_TO_DIR_DICT[event.key]
+                    move = self.dir_input
+                    self.move_key_held = event.key
                 elif event.key == K_SPACE:
                     move = "b"
             elif event.type == pygame.KEYUP:
-                if event.key in DIRECTION_INPUT_DICT:
-                    if not do_move:
-                        queued_dir_input = dir_input
-                    dir_input = (0, 0)
+                if event.key in KEY_TO_DIR_DICT and event.key == self.move_key_held:
+                    if not self.do_move:
+                        self.queued_dir_input = self.dir_input
+                    self.dir_input = (0, 0)
+                    self.move_key_held = None
 
             if move != None: # if the player is making a move
                 move_msg = ""
-                if move == (1, 0):
-                    move_msg = 'r'
-                elif move == (-1,0):
-                    move_msg = 'l'
-                elif move == (0,1):
-                    move_msg = 'd'
-                elif move == (0,-1):
-                    move_msg = 'u'
+                if move in DIR_TO_MOVE_DICT:
+                    move_msg = DIR_TO_MOVE_DICT[move]
                 elif move == 'b':
                     move_msg = 'b'
 
-                player.make_move(move_msg)
+                self.player.make_move(move_msg)
 
-
-        #---- UPDATE ----#
-        if do_move:
-            if queued_dir_input != (0,0):
-                player_char.move(queued_dir_input)
-                queued_dir_input = (0,0)
+    def update(self):
+        if self.do_move:
+            if self.queued_dir_input != (0,0):
+                self.player_char.move(self.queued_dir_input)
+                self.queued_dir_input = (0,0)
             else:
-                player_char.move(dir_input)
+                self.player_char.move(self.dir_input)
+        self.player_char.update()
 
-        player_char.update()
-
-        #---- DRAW ----#
-        screen.fill((200,200,200))
-        player_char.draw(screen)
+    def draw(self):
+        self.screen.fill((200,200,200))
+        self.player_char.draw(self.screen)
         pygame.display.flip()
-
 
 class GameBoard(object):
     """
@@ -151,5 +161,6 @@ class GameBoard(object):
     def change_tile(self, i, j, new_ele):
         self.game_grid[i][j] = new_ele
 
-if __name__ == "__main__":
-    start_game()
+def main(username, client_port, server_ip, server_port, is_server):
+    gameManager = GameManager(username, client_port, server_ip, server_port, is_server)
+    gameManager.start_game()
